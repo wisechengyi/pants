520c8f5ff521bc367dd17c1eeb289fc4d101c20d
wisechengyi <wisechengyi@gmail.com>
2016-04-27 00:09:35 -0700

    collection.of

--- src/python/pants/util/objects.py
+++ src/python/pants/util/objects.py
@@ -5,7 +5,10 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import sys
 from collections import namedtuple
+
+from pants.util.memo import memoized
 
 
 def datatype(*args, **kwargs):
@@ -20,3 +23,30 @@ def datatype(*args, **kwargs):
     def __ne__(self, other):
       return not (self == other)
   return DataType
+
+
+class Collection(object):
+  """
+  Single Collection Type.
+  """
+
+  @classmethod
+  @memoized
+  def of(cls, ext):
+    type_name = b'{}({!r})'.format(cls.__name__, ext)
+
+    # 'dependencies' field is hardcoded here. If more flexibility is needed,
+    # parameterize it as an argument.
+    ext_type = type(type_name, (cls, datatype("{}s".format(ext.__name__), ('dependencies',))), {})
+
+    # Expose the custom class type at the module level to be pickle compatible.
+    setattr(sys.modules[cls.__module__], type_name, ext_type)
+
+    return ext_type
+
+  @classmethod
+  def ext(cls):
+    raise NotImplementedError()
+
+  def __repr__(self):
+    return '{}(of={!r})'.format(self.__class__.__name__, self.ext)
