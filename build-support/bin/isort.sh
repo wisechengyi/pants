4980b84c0901c48cf7b563c9c0c246e3372d08f1
wisechengyi <wisechengyi@gmail.com>
2016-08-10 13:51:10 -0700

    rm stuff

--- build-support/bin/isort.sh
+++ build-support/bin/isort.sh
@@ -33,22 +33,5 @@ do
   esac
 done
 
-# We run differently when in the context of a git hook because otherwise nested .isort.cfg files
-# are not found, leading to false negatives. See: https://github.com/timothycrosley/isort#git-hook
-# Note that the git_hook only checks tracked files, all of which we wish to isort.  The
-# non-GIT_HOOK command line invocation unfortunately must list top-level directories in order to
-# avoid python files in virtualenvs created under build-support (Using the single . top-level dir
-# finds these files, leading to many false negatives.).
-if [[ -n "${GIT_HOOK}" ]]
-then
-  python << EOF
-import sys
-from isort.hooks import git_hook
-
-
-sys.exit(git_hook(strict=True))
-EOF
-else
-  ./pants fmt.isort -- ${isort_args[@]} --recursive src tests pants-plugins contrib examples testprojects
-fi
+./pants fmt.isort -- ${isort_args[@]} --recursive src tests pants-plugins contrib examples testprojects
 
--- build-support/bin/pre-commit.sh
+++ build-support/bin/pre-commit.sh
@@ -10,8 +10,8 @@ then
 fi
 
 echo "Checking packages" && ./build-support/bin/check_packages.sh || exit 1
-echo "Checking imports" && ./build-support/bin/isort.sh || \
-  die "To fix import sort order, run \`build-support/bin/isort.sh -f\`"
+#echo "Checking imports" && ./build-support/bin/isort.sh || \
+#  die "To fix import sort order, run \`build-support/bin/isort.sh -f\`"
 echo "Checking headers" && ./build-support/bin/check_header.sh || exit 1
 echo "Success"
 
